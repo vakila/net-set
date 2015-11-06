db5c05936b26a788bdd61af57a5b8119a9d5b73a
Anjana <anjanav@coli.uni-saarland.de>
2015-11-06 12:16:29 -0500

    Flesh out setLogic tests (passing)

--- test/test.setLogic.js
+++ test/test.setLogic.js
@@ -37,18 +37,64 @@ describe('makeCard', function() {
 describe('isSet', function(){
 
     it('should reject more or less than three cards', function(){
-      assert.equal(setLogic.isSet([1, 2]), false);
-      assert.equal(setLogic.isSet([1,2,3,4]), false);
+        var card0 = setLogic.makeCard(0,0,0,0);
+        var card1 = setLogic.makeCard(1,1,1,1);
+        var card2 = setLogic.makeCard(2,2,2,2);
+        var cardA = setLogic.makeCard(1,2,1,2);
+
+        assert.equal(setLogic.isSet(mori.vector(card0,card1)), false);
+        assert.equal(setLogic.isSet(mori.vector(card0,card1,card2,cardA)), false);
     });
 
-    it('should accept three cards that are identical');
+    it('should accept three cards that are identical', function(){
+        var card0 = setLogic.makeCard(0,0,0,0);
+        var cardA = setLogic.makeCard(1,2,1,2);
 
-    it('should accept three cards that are different in all ways');
+        assert(setLogic.isSet(mori.vector(card0,card0,card0)));
+        assert(setLogic.isSet(mori.vector(cardA,cardA,cardA)));
+    });
 
-    it('should accept three cards that share two attributes but differ on a third');
+    it('should accept three cards that are different in all ways', function(){
+        var card0 = setLogic.makeCard(0,0,0,0);
+        var card1 = setLogic.makeCard(1,1,1,1);
+        var card2 = setLogic.makeCard(2,2,2,2);
+        var cardA = setLogic.makeCard(1,2,1,2);
+        var cardB = setLogic.makeCard(0,1,0,1);
+        var cardC = setLogic.makeCard(2,0,2,0);
 
-    it('should accept three cards that share one attribute and differ on two');
+        assert(setLogic.isSet(mori.vector(card0,card1,card2)));
+        assert(setLogic.isSet(mori.vector(cardA,cardB,cardC)));
+    });
 
-    it('should reject three cards where only two share an attribute');
+    it('should accept three cards that share two attributes but differ on two', function(){
+        var card0 = setLogic.makeCard(0,0,0,0);
+        var cardX = setLogic.makeCard(0,0,1,1);
+        var cardY = setLogic.makeCard(0,0,2,2);
+
+        assert(setLogic.isSet(mori.vector(card0,cardX,cardY)));
+    });
+
+    it('should accept three cards that share one attribute and differ on three', function() {
+        var card0 = setLogic.makeCard(0,0,0,0);
+        var cardD = setLogic.makeCard(0,1,1,1);
+        var cardE = setLogic.makeCard(0,2,2,2);
+
+        assert(setLogic.isSet(mori.vector(card0,cardD,cardE)));
+    });
+
+    it('should reject three cards where only two share an attribute', function() {
+        var card0 = setLogic.makeCard(0,0,0,0);
+        var card1 = setLogic.makeCard(1,1,1,1);
+        var cardA = setLogic.makeCard(1,2,1,2);
+        var card2 = setLogic.makeCard(2,2,2,2);
+        var cardC = setLogic.makeCard(2,0,2,0);
+        var cardD = setLogic.makeCard(0,1,1,1);
+        var cardX = setLogic.makeCard(0,0,1,1);
+        var cardY = setLogic.makeCard(0,0,2,2);
+
+        assert(!setLogic.isSet(mori.vector(card0,card1,cardA)));
+        assert(!setLogic.isSet(mori.vector(card2,cardC,cardD)));
+        assert(!setLogic.isSet(mori.vector(cardD,cardX,cardY)));
+    });
 
 })
