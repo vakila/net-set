57156934fb75678d8b18a6ea62d85eccb60459e4
Anjana <anjanav@coli.uni-saarland.de>
2015-11-30 13:09:52 -0500

    Announce that server is listening

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var path = require('path');
 
-server.listen(3000);
+server.listen(3000, function() {
+  console.log("listening on port 3000");
+});
 
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'jade');
